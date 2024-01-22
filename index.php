af1e63f27b87cdf1455560969fffb917915e2699
Abdulsatar-1 <abdulsatar1@hotmail.com>
2024-01-22 23:21:56 +0200

    Version 2
    Published: 22.01.2024
    
    IN THIS VERSION:
     - Displaying JSON Data With PHP
     - Added new column into our drinks.json file
    
    NEXT VERSION:
    - Looping Through JSON Data
    - Formatting JSON out

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
-    echo file_get_contents('drinks.json');
+    $drinks =  json_decode(file_get_contents('drinks.json'));
+    // print_r($drinks);
 
+    foreach($drinks as $drink){
+        echo '<li>'.$drink->Drink_Ean.' - '.$drink->Drink_name.'</li>';
+    }
 
 ?>
